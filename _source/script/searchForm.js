1daa23de138d973235972d5b361d332814c460ad
3846masa <3846masahiro+git@gmail.com>
2017-09-02 13:50:49 +0900

    Fix JavaScript

--- _source/script/searchForm.js
+++ _source/script/searchForm.js
@@ -5,16 +5,21 @@ const searchVm = new Vue({
   }),
   computed: {
     placeholder() {
-      return `論文データベース検索（${this.researchLength} 件）`
+      return `論文データベース検索（${this.researchLength} 件）`;
     },
   },
   created() {
     fetch('https://research.miyashita.com/sitemap.xml')
-      .then((res) => res.text())
-      .then((text) => (new DOMParser()).parseFromString(text, 'application/xml'))
-      .then((xml) => {
-        this.researchLength = xml.querySelectorAll('url').length - 1;
+      .then(res => res.text())
+      .then(text => new DOMParser().parseFromString(text, 'application/xml'))
+      .then(xml => {
+        const urlList = [...xml.querySelectorAll('url')].map(
+          el => el.textContent
+        );
+        this.researchLength = urlList.filter(url =>
+          url.match(/\/[A-Z]\d+\/$/)
+        ).length;
       })
-      .catch((err) => console.error(err));
+      .catch(err => console.error(err));
   },
 });
