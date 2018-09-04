20f6db8b916778f6ad305d01e23ce04076e08058
3846masa <3846masahiro+git@gmail.com>
2018-09-04 15:54:28 +0900

    Update logomark

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -14,7 +14,7 @@ const dateTemplate = tinytime('{YYYY}/{Mo}/{DD}', {
 });
 
 const noImage =
-  'https://lh3.googleusercontent.com/ypqAGQLHvNZcOfVHv-960lbtticQC98XJfjpA471NLhFQarU8BTdSJicgTmbACdM6yocLNtgaECXRm7Aw5Eqe2PkL_B7skDuRwAbD9gce8LFlVhXP0CfLskrtXmB9M2Tejc9YG_OkJu1pp-7Z8YSpdFPOwAKyVMBrDn7U_5i_3SBkhtPNQq76T9exLzlaec6hVSsWPXTBY9xb3xCvK7SRprxLCYQcPmv-l76yiaiKeXmExm430qwBHLa2MHO1fm4YUIyca22qVHnODFTsphOdSf9tryT7yAMkcuJmeplDu4cPSera2Ac6s8ILfkX9uIgFBMIjyG2Q2VTfJ6JgCF2FBbbqdiYqnHWl_Bg9mMRW2rkqQ36uK0m0X4jR5d7CabLvXToQdVKbWrG2TZTQow0d_Lbx_zvn5JN3w5QObue_jT7naCpGiKd1jzyZwzeL7LB9gz6BlK1Hc704RlkikxVe57rTq7lnQrtsnJwaHKAKS6EY575rHVrxem6C37GkQNVTesXK1S9nkcNmmx3JoAB9uOY7yuF0fWdTQn1d4Szz7l3S2OzbKQbvsqof9l9YLt1Fb0qogRulsP4iY99SKw50BDm-hS9ViWxwHm9wWYOMw=s0';
+  'https://lh3.googleusercontent.com/-Jg11be_N1pM/W44riQZisJI/AAAAAAAAILg/RVtrOK19mVgkyZSH7zHLSBiqVX180gy-ACE0YBhgL/s400/noimage.png';
 
 /**
  * @typedef Props
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -8,7 +8,7 @@ import OptimizedImage from '~/components/OptimizedImage';
 import Link from '~/components/Link';
 
 const logoImg =
-  'https://lh3.googleusercontent.com/GWKvlXElFO095znbPRbexWAhGOoPAgdpZTGIhTUmLwCk6qiPShZaO9wHcpnuCyj9Ao14qgTixzWF6Bhgyna__6LXVvMSLgrdoiTcO4BsSpVu0M7_7Mf2xkdjDPgjUqhIHUUIs3BbgIQvOyCxVXf3gNI-K5N_W37xqfcWhED68B26bfit31qx0nGWyfGwPKT8Y9A2yWJrQPbfkWaOJB0Qqu4Swr3zYCrozrXVRtqbe0BtXILmmTmGeVQ9Ed7YgVyJIoSPFCwIXnkDijdUk2-ixB-NeBZ0IN_TkMh1bxbLUswN9ZwBcMStCM3KZUS6N0Nq6R2S8A3ISgBLN8YU7hT48Fr5y6EBd_BHmXpXBphXVCPGocGCz1AAutaNK1ocpIrTcuqLiXiSRJOm4Ae5ErX_h2EsXHmpEelJHx7d3Em2UkkI0v2rCRQQfkgxl-SFIae272f6ggj778HotznlLrHr1cSQI0uJOmK0cNRc9iUF3-TZFrsOz65KzDfSyNcIplQDpn8QGVF9HmdysmnWlXVpA-4kHzn6s6ctxcnAbdBuhq68CNvbs8ILpkXlY6Z3C1DkxRQzQ2v612ktTmR7b2nQKBF4TgWoOmD0CpVCqN5dGg=s0';
+  'https://lh3.googleusercontent.com/-QPw7WLxdN9M/W44p1fOm4XI/AAAAAAAAILA/4JiqVbXKQaYElaAR6j4jFhE2bZVUn_f6wCE0YBhgL/s0/sublogo1.png';
 
 const PageNav = () => (
   <nav className={styles.base}>
