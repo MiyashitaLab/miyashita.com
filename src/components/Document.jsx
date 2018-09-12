dcd0f2d74d39298c3edbe08d77d9a886dc9e3990
3846masa <3846masahiro+git@gmail.com>
2018-09-12 19:36:56 +0900

    Add favicon

--- src/components/Document.jsx
+++ src/components/Document.jsx
@@ -6,6 +6,8 @@ const Document = ({ Html, Head, Body, children, renderMeta }) => (
       <meta charSet="utf-8" />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" />
       <style data-ssr dangerouslySetInnerHTML={{ __html: renderMeta.styles }} />
       <script async src="https://www.googletagmanager.com/gtag/js" />
     </Head>
